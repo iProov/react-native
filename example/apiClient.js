4e7de2a023defcf646689585a405663ce966518f
Laolu Animashaun <laolu.animashaun@iproov.com>
2022-03-24 10:06:03 +0000

    JS API Client (#4)
    * WIPS
    
    * Export iProov event keys
    
    * Clean up public API
    
    * Clean JS Api client
    
    * Clean up options
    
    * Wips
    
    * Clean up
    
    * implement validate and invalidate api calls

--- example/apiClient.js
+++ example/apiClient.js
@@ -1,3 +1,4 @@
+import RNFetchBlob from 'rn-fetch-blob'
 
 export class ApiClient {
 
@@ -27,4 +28,56 @@ export class ApiClient {
     return response.json();
   }
 
+   async enrolPhoto(token, image, photoSource) {
+
+    let form = [
+      {name: 'api_key', data: this.apiKey},
+      {name: 'secret', data: this.secret },
+      {name: 'rotation', data: '0' },
+      {name: 'token', data: token },
+      { name: 'source', data: 'oid'},
+      { name: 'image', filename: 'image.jpeg', type: 'image/jpeg', data: image } 
+    ]
+
+    const response = await RNFetchBlob.fetch('POST', `${this.baseUrl}claim/enrol/image`, {'Content-Type': 'multipart/form-data; boundary=-------kjqdgfljhsgdfljhgsdlfjhgasdf'}, form)
+
+    return response.json()
+  }
+
+  async validate(token, userId) {
+
+    const response = await fetch(this.baseUrl + 'claim/verify/validate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        api_key: this.apiKey,
+        secret: this.secret,
+        user_id: userId,
+        token: token,
+        assurance_type: assuranceType,
+        ip: '127.0.0.1',
+        client: 'android'
+      })
+    });
+  
+    return response.json();
+  }
+
+  async invalidate(token, reason) {
+
+    const response = await fetch(this.baseUrl + 'claim/${token}/invalidate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        reason: reason
+      })
+    });
+  
+    return response.json();
+  }
+  
 }
