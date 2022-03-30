fdc929be1688c9835083e7751270aab73f6a778b
Laolu Animashaun <laolu.animashaun@iproov.com>
2022-03-30 20:23:58 +0100

    JS event listener registration more DRY

--- src/iProov.js
+++ src/iProov.js
@@ -30,62 +30,21 @@ function getNativeEventEmitterInstace() {
 
 function registerDelegateListeners(listener) {
   const eventEmitter = getNativeEventEmitterInstace()
-  eventEmitter.addListener(EVENT_CONNECTING, (event) => {
-    listener({ event: EVENT_CONNECTING, params: event })
-  })
+  const events = [
+    EVENT_CONNECTING,
+    EVENT_CONNECTED,
+    EVENT_PROCESSING,
+    EVENT_SUCCESS,
+    EVENT_FAILURE,
+    EVENT_CANCELLED
+  ]
 
-  eventEmitter.addListener(EVENT_CONNECTED, (event) => {
-    listener({
-      event: EVENT_CONNECTED,
-      params: event
+  events.forEach((eventType) => {
+    eventEmitter.addListener(eventType, (event) => {
+      listener({
+        event: eventType,
+        params: event
+      })
     })
   })
-
-  eventEmitter.addListener(EVENT_PROCESSING, (event) => {
-    listener({
-      event: EVENT_PROCESSING,
-      params: event
-    })
-  })
-
-  eventEmitter.addListener(EVENT_SUCCESS, (event) => {
-    listener({
-      event: EVENT_SUCCESS,
-      params: event
-    })
-    removeAllListeners(eventEmitter)
-  })
-
-  eventEmitter.addListener(EVENT_FAILURE, (event) => {
-    listener({
-      event: EVENT_FAILURE,
-      params: event
-    })
-    removeAllListeners(eventEmitter)
-  })
-
-  eventEmitter.addListener(EVENT_ERROR, (event) => {
-    listener({
-      event: EVENT_ERROR,
-      params: event
-    })
-    removeAllListeners(eventEmitter)
-  })
-
-  eventEmitter.addListener(EVENT_CANCELLED, (event) => {
-    listener({
-      event: EVENT_CANCELLED,
-      params: event
-    })
-    removeAllListeners(eventEmitter)
-  })
-}
-
-function removeAllListeners(eventEmitter) {
-  eventEmitter.removeAllListeners(EVENT_CONNECTING)
-  eventEmitter.removeAllListeners(EVENT_CONNECTED)
-  eventEmitter.removeAllListeners(EVENT_PROCESSING)
-  eventEmitter.removeAllListeners(EVENT_SUCCESS)
-  eventEmitter.removeAllListeners(EVENT_FAILURE)
-  eventEmitter.removeAllListeners(EVENT_CANCELLED)
 }
